29cd2793d6e3463ea41e89b5d63c2afb2b51145c
orb li <mail@orbb.li>
2023-01-30 16:54:45 +0800

    align with requirement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BinanceLv2Digester digester = new BinanceLv2Digester("ETHUSDT");
         digester.start();
-        new Thread(new OrderbookPrinter(digester, 10, 1000)).start();
+        new Thread(new OrderbookPrinter(digester, 10, 10000)).start();
         System.out.println("main finished");
     }
 }
